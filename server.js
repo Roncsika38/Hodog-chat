c4fc4416333c62110891b7c312d5bf48d88e76b5
Zoltán Kiss <roncsika635@gmail.com>
2026-04-20 05:49:38 +0200

    Refactor user management and message handling

--- server.js
+++ server.js
@@ -1,91 +1,32 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static("public"));
-
 let users = {};
-let messages = [];
-let banned = [];
-
-const ADMINS = ["roncsika38"];
 
 io.on("connection", (socket) => {
 
-    socket.on("join", (username) => {
+  socket.on("join", (username) => {
+    socket.username = username;
 
-        if (banned.includes(username)) {
-            socket.emit("banned");
-            return;
-        }
+    users[socket.id] = {
+      name: username,
+      role: username === "roncsika38" ? "admin" : "user"
+    };
 
-        socket.username = username;
-        users[socket.id] = username;
+    io.emit("users", Object.values(users));
+  });
 
-        socket.emit("chatHistory", messages);
-        io.emit("users", Object.values(users));
+  socket.on("message", (data) => {
+    const user = users[socket.id];
+
+    io.emit("message", {
+      username: user.name,
+      role: user.role,
+      msg: data.msg,
+      time: new Date().toLocaleTimeString()
     });
+  });
 
-    socket.on("message", (data) => {
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+    io.emit("users", Object.values(users));
+  });
 
-        if (ADMINS.includes(socket.username)) {
-
-            if (data.msg === "/clear") {
-                messages = [];
-                io.emit("clearChat");
-                return;
-            }
-
-            if (data.msg.startsWith("/kick ")) {
-                let target = data.msg.split(" ")[1];
-
-                for (let id in users) {
-                    if (users[id] === target) {
-                        io.to(id).emit("kicked");
-                        delete users[id];
-                    }
-                }
-
-                io.emit("users", Object.values(users));
-                return;
-            }
-
-            if (data.msg.startsWith("/ban ")) {
-                let target = data.msg.split(" ")[1];
-                banned.push(target);
-
-                for (let id in users) {
-                    if (users[id] === target) {
-                        io.to(id).emit("banned");
-                        delete users[id];
-                    }
-                }
-
-                io.emit("users", Object.values(users));
-                return;
-            }
-        }
-
-        const msgData = {
-            username: socket.username,
-            msg: data.msg,
-            time: new Date().toLocaleTimeString()
-        };
-
-        messages.push(msgData);
-        if (messages.length > 100) messages.shift();
-
-        io.emit("message", msgData);
-    });
-
-    socket.on("disconnect", () => {
-        delete users[socket.id];
-        io.emit("users", Object.values(users));
-    });
 });
-
-server.listen(process.env.PORT || 3000);
