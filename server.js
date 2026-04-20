3a1339ef27e2b7eca7ae60e2ea740fdd57fbd90d
Zoltán Kiss <roncsika635@gmail.com>
2026-04-20 05:58:59 +0200

    Refactor socket event handling in server.js

--- server.js
+++ server.js
@@ -1,32 +1,47 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
 let users = {};
 
 io.on("connection", (socket) => {
 
-  socket.on("join", (username) => {
-    socket.username = username;
+    socket.on("join", (username) => {
+        socket.username = username;
 
-    users[socket.id] = {
-      name: username,
-      role: username === "roncsika38" ? "admin" : "user"
-    };
+        users[socket.id] = {
+            name: username,
+            role: username === "roncsika38" ? "admin" : "user"
+        };
 
-    io.emit("users", Object.values(users));
-  });
-
-  socket.on("message", (data) => {
-    const user = users[socket.id];
-
-    io.emit("message", {
-      username: user.name,
-      role: user.role,
-      msg: data.msg,
-      time: new Date().toLocaleTimeString()
+        io.emit("users", Object.values(users));
     });
-  });
 
-  socket.on("disconnect", () => {
-    delete users[socket.id];
-    io.emit("users", Object.values(users));
-  });
+    socket.on("message", (data) => {
+        let user = users[socket.id];
 
+        io.emit("message", {
+            username: user.name,
+            role: user.role,
+            msg: data.msg,
+            time: new Date().toLocaleTimeString()
+        });
+    });
+
+    socket.on("clear", () => {
+        let user = users[socket.id];
+        if (user.role === "admin") {
+            io.emit("clear");
+        }
+    });
+
+    socket.on("disconnect", () => {
+        delete users[socket.id];
+        io.emit("users", Object.values(users));
+    });
 });
+
+http.listen(3000);
