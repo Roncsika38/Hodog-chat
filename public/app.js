0e90dc5f753e07d6afdd0d4448114f13dd85a4e6
Zoltán Kiss <roncsika635@gmail.com>
2026-04-21 01:00:05 +0200

    Refactor chat functionality and remove auth methods
    Removed login and registration functions, and adjusted message handling.

--- public/app.js
+++ public/app.js
@@ -1,70 +1,13 @@
 const socket = io();
 
-let username = "";
-
-function login() {
-  const user = document.getElementById("user").value;
-  const pass = document.getElementById("pass").value;
-
-  fetch("/login", {
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({username:user,password:pass})
-  })
-  .then(res=>res.json())
-  .then(data=>{
-    if(data.success){
-      username = user;
-      startChat();
-    } else alert(data.error);
-  });
+function sendMsg() {
+  const input = document.getElementById("msg");
+  socket.emit("chat message", input.value);
+  input.value = "";
 }
 
-function register(){
-  const user = document.getElementById("user").value;
-  const pass = document.getElementById("pass").value;
-
-  fetch("/register", {
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({username:user,password:pass})
-  })
-  .then(res=>res.json())
-  .then(data=>{
-    alert(data.success ? "Siker!" : data.error);
-  });
-}
-
-function startChat(){
-  document.getElementById("login").style.display="none";
-  document.getElementById("chat").style.display="flex";
-  document.getElementById("username").innerText=username;
-
-  socket.emit("join", username);
-}
-
-function send(){
-  const msg = document.getElementById("msg").value;
-  socket.emit("message", msg);
-  document.getElementById("msg").value="";
-}
-
-socket.on("message", (data)=>{
+socket.on("chat message", msg => {
   const div = document.createElement("div");
-  div.className="msg";
-  div.innerText = data.username + ": " + data.text;
+  div.innerText = msg;
   document.getElementById("messages").appendChild(div);
 });
-
-socket.on("loadMessages", (msgs)=>{
-  msgs.forEach(m=>{
-    const div = document.createElement("div");
-    div.className="msg";
-    div.innerText = m.username + ": " + m.text;
-    document.getElementById("messages").appendChild(div);
-  });
-});
-
-function logout(){
-  location.reload();
-}
