350dbbadc3923f4f344bbad55b52d6febb5ea486
Zoltán Kiss <roncsika635@gmail.com>
2026-04-21 00:39:39 +0200

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,5 +1,18 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+server.listen(3000, () => {
+  console.log("SERVER OK 🔥");
 });
