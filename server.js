d35afbe052bcc4d1a163f54429b1843511aa9cda
Zoltán Kiss <roncsika635@gmail.com>
2026-04-24 23:50:39 +0200

    Add additional server listener on port 3000

--- server.js
+++ server.js
@@ -47,3 +47,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Server fut a porton:", PORT);
 });
+server.listen(3000, () => {
+  console.log("Server fut a 3000-es porton");
+});
