02ba4632e82371b693eaf39268faab55afd38ff1
Zoltán Kiss <roncsika635@gmail.com>
2026-04-25 17:57:14 +0200

    Refactor socket event handling and logging

--- server.js
+++ server.js
@@ -8,52 +8,25 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let bannedUsers = [];
-
 io.on("connection", (socket) => {
-  console.log("user connected");
+  console.log("User connected");
 
   socket.on("join", (username) => {
-    if (bannedUsers.includes(username)) {
-      socket.emit("banned");
-      return;
-    }
-
     socket.username = username;
-
-    io.emit("chat message", {
-      user: "SYSTEM",
-      text: username + " csatlakozott",
-      id: Date.now()
-    });
+    io.emit("message", `${username} csatlakozott`);
   });
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", {
-      user: socket.username || "Ismeretlen",
-      text: msg,
-      id: Date.now()
-    });
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", `${socket.username}: ${msg}`);
   });
 
-  socket.on("delete message", (id) => {
-    io.emit("delete message", id);
-  });
-
-  socket.on("ban user", (username) => {
-    bannedUsers.push(username);
-
-    io.emit("chat message", {
-      user: "SYSTEM",
-      text: username + " kitiltva",
-      id: Date.now()
-    });
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      io.emit("message", `${socket.username} kilépett`);
+    }
   });
 });
 
-// ✅ CSAK EZ MARAD
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log("Server fut a porton:", PORT);
+server.listen(3000, () => {
+  console.log("Server fut: http://localhost:3000");
 });
