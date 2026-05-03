3e27ad9682005eca205c3cfe652117717693982e
Zoltán Kiss <roncsika635@gmail.com>
2026-05-03 14:32:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const io = new Server(server, { cors: { origin: '*' } });
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
-
+app.use(express.static('public'));
 // ===== MongoDB =====
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log('MongoDB connected'))
